accb556055b060a702f11165289833f00ae77572
Vladimir Markelov <vladimir.markelov@nexenta.com>
2015-09-25 16:44:45 -0700

    add min and max for int

---
+++ math_util.go
@@ -0,0 +1,17 @@
+package clui
+
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func MinInt(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
