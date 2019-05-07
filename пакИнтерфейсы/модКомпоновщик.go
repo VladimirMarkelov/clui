5cbc3efc3ced8b1f3c24ebd20c3a4a8e79bb2d9a
prospero78su <prospero.78.su@gmail.com>
2019-05-07 12:49:05 +0300

    289 Начало добавления интерфейса компановщика

---
+++ пакИнтерфейсы/модКомпоновщик.go
@@ -0,0 +1,8 @@
+package пакИнтерфейсы
+
+// PackType sets how to pack controls inside its parent. Can be Vertical or
+// Horizontal
+type PackType int
+
+// Align is text align: left, right and center
+type Align int
